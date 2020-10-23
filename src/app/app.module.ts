70f6038982da66f944b6644720ed73ddc37d75eb
nada <nadaaskora@gmail.com>
2020-10-23 11:46:32 +0200

    angular routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {AppRoutingModule } from './content/routes/routes.module'
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContentComponent } from './content/content.component';
@@ -12,11 +12,14 @@ import { ItemComponent } from './content/item/item.component';
     AppComponent,
     ContentComponent,
     ListComponent,
-    ItemComponent
+    ItemComponent,
+    
+
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/content/routes/routes.module.ts
+++ src/app/content/routes/routes.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { from } from 'rxjs';
 import {  ContentComponent} from '../content.component';
+import {ListComponent } from '../list/list.component';
+import { ItemComponent} from '../item/item.component';
 
 const routes: Routes = [
   { path: 'content', component: ContentComponent },
+  { path: 'content/list', component: ListComponent },
+  { path: 'content/item', component: ItemComponent },
   { path: 'content/$id', component: ContentComponent }
 ];
 
